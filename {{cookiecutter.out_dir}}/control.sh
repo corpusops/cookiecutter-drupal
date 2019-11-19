587bc540d2a95d043393dc11cc1702db9d2a9e21
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2019-11-19 23:20:56 +0100

    fix action

--- {{cookiecutter.out_dir}}/control.sh
+++ {{cookiecutter.out_dir}}/control.sh
@@ -516,7 +516,7 @@ do_main() {
     if [[ -n $@ ]];then shift;fi
     set_dc
     case $action in
-        $actions) do_$action $@;;
+        $actions) do_$action "$@";;
         *) do_usage;;
     esac
 }
