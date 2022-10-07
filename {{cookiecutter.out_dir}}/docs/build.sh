0acb2e155bb9bec5a8261aec4c1221f243d9fa50
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2022-10-07 22:47:09 +0200

    last fixes

--- {{cookiecutter.out_dir}}/docs/build.sh
+++ {{cookiecutter.out_dir}}/docs/build.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env bash
-cd $(dirname $(readlink -f $0))/..
+set -e
+set -o pipefail
+
+if [ "x${SDEBUG-}" != "x" ];then set -x;fi
+readlinkf() {
+    if ( uname | egrep -iq "darwin|bsd" );then
+        if ( which greadlink 2>&1 >/dev/null );then
+            greadlink -f "$@"
+        elif ( which perl 2>&1 >/dev/null );then
+            perl -MCwd -le 'print Cwd::abs_path shift' "$@"
+        elif ( which python 2>&1 >/dev/null );then
+            python -c 'import os,sys;print(os.path.realpath(sys.argv[1]))' "$@"
+        fi
+    else
+        readlink -f "$@"
+    fi
+}
+cd $(dirname $(readlinkf $0))/..
 ./control.sh make_docs
 # vim:set et sts=4 ts=4 tw=80:
